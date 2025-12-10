b310d3d914281cdbe17a1552f7c9d1ddd05c4601
Kirill Vetrov <kirill.vetrov.mail@gmail.com>
2025-12-10 12:14:53 +0200

    Initial Commit

---
+++ simple web server/simple_web_serverApp.swift
@@ -0,0 +1,17 @@
+//
+//  simple_web_serverApp.swift
+//  simple web server
+//
+//  Created by Кирилл Ветров on 12/10/25.
+//
+
+import SwiftUI
+
+@main
+struct simple_web_serverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
